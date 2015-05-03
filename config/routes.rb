099ce16c7b2d606566c0136bf76b358c0b567f04
Dang Minh <betonamei@hotmail.com>
2015-05-03 18:06:47 +0900

    add admin page

--- config/routes.rb
+++ config/routes.rb
@@ -1,48 +1,40 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'static_pages/index'
+  end
+
   root 'static_pages#index'
 
   devise_for :users , :skip => [:sessions, :passwords, :registrations]
   as :user do
+    #new_user_session
     get   'signin' => 'devise/sessions#new', :as => :new_user_session
-
+    #user_session
     post  'signin' => 'devise/sessions#create', :as => :user_session
-    
     #destroy_user_session DELETE /users/sign_out(.:format)      devise/sessions#destroy
     match 'signout' => 'devise/sessions#destroy', :as => :destroy_user_session, :via => Devise.mappings[:user].sign_out_via
-    
     #user_password POST   /users/password(.:format)      devise/passwords#create
     get   'user_password' => 'devise/passwords#create', :as => :user_password
-    
     #new_user_password GET    /users/password/new(.:format)  devise/passwords#new
     get   'new_user_password' => 'devise/passwords#new', :as => :new_user_password 
-    
     #edit_user_password GET    /users/password/edit(.:format) devise/passwords#edit
     get   'edit_password' => 'devise/passwords#edit', :as => :edit_user_password
-
     #PATCH  /users/password(.:format)      devise/passwords#update
     patch '/users/password(.:format)' => 'devise/passwords#update' 
-
     #PUT    /users/password(.:format)      devise/passwords#update
     put   '/users/password(.:format)' => 'devise/passwords#update'
-    
     #cancel_user_registration GET    /users/cancel(.:format)        devise/registrations#cancel
     get   'cancel_user_registration' => 'devise/registrations#cancel', :as => :cancel_user_registration
-    
     #user_registration POST   /users(.:format)               devise/registrations#create
     post  'user_registration' => 'devise/registrations#create'
-
     #new_user_registration GET    /users/sign_up(.:format)       devise/registrations#new
     get   'sign_up' => 'devise/registrations#new', :as => :new_user_registration
-    
     #edit_user_registration GET    /users/edit(.:format)          devise/registrations#edit
     get   'edit_user_registration' => 'devise/registrations#edit', :as => :edit_user_registration
-
     #PATCH  /users(.:format)               devise/registrations#update
     patch '/users(.:format)' => 'devise/registrations#update'
-    
     #PUT    /users(.:format)               devise/registrations#update
     put   'users(.:format)' => 'devise/registrations#update'
-    
     #DELETE /users(.:format)               devise/registrations#destroy
     delete '/users(.:format)' => 'devise/registrations#destroy'
   end
